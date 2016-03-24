bd3a3b05826034d6267498b92d8d7eb63d15c9c5
Nelson Perez <nperez@nisum.com>
2016-03-24 11:49:51 -0300

    Update script from MA script

--- http-request.groovy
+++ http-request.groovy
@@ -1,33 +1,45 @@
 /*Coment*/
+def url = new URL('http://10.10.10.43:8081/artifactory/CodingApp/com/nisum/CodingApp/')
+def listDirs   = []
+def connection = url.openConnection()
+connection.requestMethod = 'GET'
+if (connection.responseCode == 200) {
+   def dirs
+   html = connection.content.text
 
-//def url = new URL('http://groovy.codehaus.org/')
-//def connection = url.openConnection()
-//connection.requestMethod = 'GET'
-//if (connection.responseCode == 200) {
-	//println connection.responseCode
-	//println connection.responseMessage
-	//println connection.requestMethod
-  //println connection.content.text
-	//println connection.contentType
-	//println connection.lastModified
+   html.eachLine{
+        if (it =~ /a href\=\"\w+/){
+           dirs = (it =~ /\"\>(.+)\/\<\/a\>/)[ 0 ][ 1 ]
+           listDirs.push(dirs)
+        }
+   }
+}else {
+        println 'An error occurred: ' + connection.responseCode + ' ' + connection.responseMessage
+}
+//println listDirs
+def urlWar
+def listWars = []
 
+listDirs.each{
+        urlWar = new URL('http://10.10.10.43:8081/artifactory/CodingApp/com/nisum/CodingApp/'+it)
+        def wardir = it
+        def connectionWar = urlWar.openConnection()
+        connectionWar.requestMethod = 'GET'
 
-	//connection.headerFields.each {
-	//		println "> ${it}"
-	//}
+        if (connectionWar.responseCode == 200) {
+        def wars
+           html = connectionWar.content.text
 
-	//html = connection.content.text
-	//html.grep() {${2/(.+)/(?1: )/}obj${2/(.+)/(?1: ->)/}
+           html.eachLine{
 
-	//}
-
-//} else {
-//	println 'An error occurred: ' + connection.responseCode + ' ' + connection.responseMessage
-//}
-
-
-def html = "'<div class='logo' style='background-image:url(img/logos/netflix.png.pagespeed.ce.waguTf5Mzv.png)'></div>"
-
-def matcher = html =~ /.*logos\/().*/
-
-println matcher
+                if (it =~ /\.war\"/){
+                   wars = (it =~ /\"(.+)\.war\"/)[ 0 ][ 1 ]
+                   finalUrl = "$wardir-$wars"
+                   listWars.push(finalUrl)
+                }
+           }
+        }
+}
+//println listWars
+println listWars.join(",")
+return listWars.join(",")
